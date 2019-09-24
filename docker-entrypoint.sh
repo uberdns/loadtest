6f2a2f914ee7871c44c762cf36adf2827e3f8f67
Alex Conrey <alex@conrey.us>
2019-09-23 21:03:41 -0700

    entrypoint updates for creating a user and api key

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,11 +29,42 @@ pushd admin-server
 
 pip3 install -r requirements.txt
 
+## Make migrations
+python3 manage.py makemigrations
+python3 manage.py makemigrations dns
+
 ## Run migrations
 python3 manage.py migrate
 
 ## Start server
-python3 manage.py runserver 2&>1 &>admin-server.log &
+python3 manage.py runserver 2>&1 1>admin-server.log &
+
+cat <<EOF > create_user.py
+# this must happen before we try to import objects
+import django
+django.setup()
+
+from django.contrib.auth.models import User
+user = User.objects.create_user('loadtest', password='loadtest')
+user.is_superuser = False
+user.is_staff = False
+user.save()
+
+from lsofadmin.dns.models import Domain
+domain = Domain.objects.create_domain("lsof.top")
+EOF
+
+# https://stackoverflow.com/questions/26082128/improperlyconfigured-you-must-either-define-the-environment-variable-django-set
+DJANGO_SETTINGS_MODULE=lsofadmin.settings python3 create_user.py
+if [ ! $? -eq 0 ]; then
+    exit 1
+fi
+
+# Create the API key we use in the loadtest requests
+API_KEY="abc123"
+CURDATE=$(date "+%Y-%m-%d %T.%N")
+SQL_QUERY="INSERT INTO authtoken_token (\`key\`, created, user_id) VALUES ('${API_KEY}', '${CURDATE}', 1)"
+mysql -e "${SQL_QUERY}" lsofadmin
 
 popd
 
